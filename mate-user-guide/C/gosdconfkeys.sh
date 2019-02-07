ee873c2d7836bbc32edc7ca3e0f7436da37a33b5
rbuj <robert.buj@gmail.com>
2019-02-07 19:45:13 +0100

    Upgrade the manual to docbook 5.0
    To upgrade the manual:
    sudo dnf install -y docbook5-schemas
    xsltproc --output index-new.docbook /usr/share/xml/docbook5/stylesheet/upgrade/db4-upgrade.xsl index.docbook
    xsltproc --output legal-new.xml /usr/share/xml/docbook5/stylesheet/upgrade/db4-upgrade.xsl legal.xml
    (...)
    
    To validate the manual:
    xmllint --noout --relaxng /usr/share/xml/docbook5/schema/rng/5.0/docbookxi.rng mate-user-guide/C/index.docbook
    jing /usr/share/xml/docbook5/schema/rng/5.0/docbookxi.rng mate-user-guide/C/index.docbook
    yelp-check validate mate-user-guide/C/index.docbook
    
    To view the manual:
    yelp file:///full_path/index.docbook
    
    Note: docbook5-schemas should be installed in your system in order to view the manual (DEPENDENCY)

--- mate-user-guide/C/gosdconfkeys.sh
+++ mate-user-guide/C/gosdconfkeys.sh
@@ -18,14 +18,20 @@ if [ ! -d "mate-desktop" ]; then
   git clone https://github.com/mate-desktop/mate-desktop.git
 fi
 cat << EOF > $OUTPUT
-  <sect1 id="mate-dconf-list">
-    <title>List of Dconf Keys of MATE Desktop</title>
+<?xml version="1.0" encoding="utf-8"?>
+<?db.chunk.max_depth 3?>
+<section xmlns="http://docbook.org/ns/docbook"
+         xmlns:db="http://docbook.org/ns/docbook"
+         xmlns:xlink="http://www.w3.org/1999/xlink"
+         xmlns:its="http://www.w3.org/2005/11/its"
+         version="5.0" its:version="2.0" xml:id="mate-dconf-list" xml:lang="en">
+    <info><title>List of Dconf Keys of MATE Desktop</title></info>
 EOF
 for file in mate-desktop/schemas/*.gschema.xml.in; do
   xsltproc gosdconfkeys.xsl $file >> $OUTPUT
 done
 cat << EOF >> $OUTPUT
-  </sect1>
+  </section>
 EOF
 xmllint -format $OUTPUT > $OUTPUT.aux
 mv $OUTPUT.aux $OUTPUT
